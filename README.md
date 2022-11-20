c84254b2721a606caa31343b3d4c5537a60b584e
Ilkil <101128575+Ilkil@users.noreply.github.com>
2022-11-20 18:54:22 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Gyromite
+
+
+
+## __TODO__
+
+- [ ] Cordes
+- [ ] Deplacement des colonnes
+- [x] IA Smicks
+- [ ] Gestion des points de vie
+- [ ] Systeme de mort
+- [ ] Gagner le jeu
+- [ ] Ramasser les bombes
+- [ ] Systeme de score
+- [ ] Timer
+- [ ] Ecrasement (par les colonnes)
---
+++ codeDeBase/src/modele/deplacements/IA.java
@@ -0,0 +1,43 @@
+package modele.deplacements;
+
+import modele.plateau.Entite;
+import modele.plateau.EntiteDynamique;
+
+public class IA extends RealisateurDeDeplacement {
+    protected boolean realiserDeplacement() {
+        boolean ret = false;
+
+        for (EntiteDynamique e : lstEntitesDynamiques) {
+            Entite eDroite = e.regarderDansLaDirection(Direction.droite);
+            Entite eGauche = e.regarderDansLaDirection(Direction.gauche);
+            if(e.left){
+                if(e.regarderDansLaDirection(Direction.dbg) == null){
+                    e.left = false;
+                    ret = true;
+                    return ret;
+                }
+                else if(eGauche != null && eDroite == null && e.regarderDansLaDirection(Direction.dbd) != null){
+                    e.left = false;
+                    e.avancerDirectionChoisie(Direction.droite);
+                    ret = true;
+                }
+                else e.avancerDirectionChoisie(Direction.gauche);
+            }
+            else{
+                if(e.regarderDansLaDirection(Direction.dbd) == null){
+                    e.left = true;
+                    ret = true;
+                    return ret;
+                }
+                else if(eDroite != null && eGauche == null && e.regarderDansLaDirection(Direction.dbg) != null){
+                    e.left = true;
+                    e.avancerDirectionChoisie(Direction.gauche);
+                    ret = true;
+                }
+                else e.avancerDirectionChoisie(Direction.droite);
+            }
+        }
+        return ret;
+    } // TODO
+
+}
